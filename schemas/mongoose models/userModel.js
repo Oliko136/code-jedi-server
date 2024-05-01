b7b3e688984b765420e1869a8a2efe2ca9eb730e
Oliko136 <olchick136@gmail.com>
2024-05-01 22:38:54 +0400

    User mongoose model created

---
+++ schemas/mongoose models/userModel.js
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+import handleMongooseError from "../../hooks/handleMongooseError.js";
+import { THEME_TYPES } from "../../constants/themeTypes.js";
+
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Name is required'],
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+        },
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            unique: true,
+        },
+        theme: {
+            type: String,
+            enum: THEME_TYPES,
+            default: "dark",
+        },
+        token: {
+            type: String,
+            default: null,
+        }
+    },
+    { versionKey: false }
+)
+
+userSchema.post('save', handleMongooseError);
+
+const User = model('user', userSchema);
+
+export default User;
