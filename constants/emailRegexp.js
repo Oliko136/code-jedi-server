49a415a5c9630b63295065299ffe4c4116e59a26
Elena Melnyk <100163385+elenamel1803@users.noreply.github.com>
2024-05-03 01:14:15 +0300

    update userSchema, userModel, emailRegexp

--- constants/emailRegexp.js
+++ constants/emailRegexp.js
@@ -1,4 +1,4 @@
-const emailRegexp =
+const EMAIL_REGEXP =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-export default emailRegexp;
+export default EMAIL_REGEXP;
--- schemas/joiSchemas/userSchemas.js
+++ schemas/joiSchemas/userSchemas.js
@@ -1,31 +1,39 @@
 import Joi from "joi";
-import emailRegexp from "../../constants/emailRegexp.js";
+import EMAIL_REGEXP from "../../constants/emailRegexp.js";
 import THEME_TYPES from "../../constants/themeTypes.js";
 
 export const userRegistrationSchema = Joi.object({
-  name: Joi.string().required().messages({
+  name: Joi.string().min(2).max(32).required().messages({
     "string.base": "Name must be a string",
+    "string.min": "Name must have a minimum length of {#limit} symbols",
+    "string.max": "Name must have a maximum length of {#limit} symbols",
     "any.required": "Missing required field name",
   }),
-  email: Joi.string().pattern(emailRegexp).required().messages({
+  email: Joi.string().pattern(EMAIL_REGEXP).required().messages({
     "string.base": "Email must be a string",
     "string.pattern.base": "Incorrect email format",
     "any.required": "Missing required field email",
   }),
-  password: Joi.string().required().messages({
+  password: Joi.string().min(8).max(64).pattern(/^\S+$/).required().messages({
     "string.base": "Password must be a string",
+    "string.min": "Password must have a minimum length of {#limit} symbols",
+    "string.max": "Password must have a maximum length of {#limit} symbols",
+    "string.pattern.base": "Password must not contain spaces",
     "any.required": "Missing required field password",
   }),
 });
 
 export const userLoginSchema = Joi.object({
-  email: Joi.string().pattern(emailRegexp).required().messages({
+  email: Joi.string().pattern(EMAIL_REGEXP).required().messages({
     "string.base": "Email must be a string",
     "string.pattern.base": "Incorrect email format",
     "any.required": "Missing required field email",
   }),
-  password: Joi.string().required().messages({
+  password: Joi.string().min(8).max(64).pattern(/^\S+$/).required().messages({
     "string.base": "Password must be a string",
+    "string.min": "Password must have a minimum length of {#limit} symbols",
+    "string.max": "Password must have a maximum length of {#limit} symbols",
+    "string.pattern.base": "Password must not contain spaces",
     "any.required": "Missing required field password",
   }),
 });
@@ -42,14 +50,19 @@ export const updateUserThemeSchema = Joi.object({
 });
 
 export const updateUserProfileSchema = Joi.object({
-  name: Joi.string().messages({
+  name: Joi.string().min(2).max(32).messages({
     "string.base": "Name must be a string",
+    "string.min": "Name must have a minimum length of {#limit} symbols",
+    "string.max": "Name must have a maximum length of {#limit} symbols",
   }),
-  email: Joi.string().pattern(emailRegexp).messages({
+  email: Joi.string().pattern(EMAIL_REGEXP).messages({
     "string.base": "Email must be a string",
     "string.pattern.base": "Incorrect email format",
   }),
-  password: Joi.string().messages({
+  password: Joi.string().min(8).max(64).pattern(/^\S+$/).messages({
     "string.base": "Password must be a string",
+    "string.min": "Password must have a minimum length of {#limit} symbols",
+    "string.max": "Password must have a maximum length of {#limit} symbols",
+    "string.pattern.base": "Password must not contain spaces",
   }),
 });
