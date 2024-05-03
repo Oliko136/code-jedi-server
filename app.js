40d0c605210f17d286684971a111e6224a970f2b
Oliko136 <olchick136@gmail.com>
2024-05-04 00:34:32 +0400

    Auth endpoints documentation assembled

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import morgan from "morgan";
 import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert {"type": "json"};
+
 import authRouter from "./routes/authRoutes.js";
 
 const { DB_HOST, PORT } = process.env;
@@ -14,6 +17,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/auth", authRouter);
 
 app.use((_, res) => {
